f8fe86c43c8e2fe7cba0536d6703887a44ce99da
Lexi Han <108366350+lhapollo@users.noreply.github.com>
2026-08-24 12:59:42 -0400

    Add: sort-an-array - submission-0

---
+++ Data Structures & Algorithms/sort-an-array/submission-0.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        priority_queue<int, vector<int>, greater<int>> minHeap; 
+        for (auto num: nums) minHeap.push(num);
+        for (int i = 0; i < nums.size(); i++) {
+            nums[i] = minHeap.top();
+            minHeap.pop();
+        } 
+        return nums; 
+    }
+};
